7fc51b26747d80118428753f4318abcad1a524a1
Elliot-Dorey <dorey.elliot@gmail.com>
2022-10-24 13:48:05 +0100

    Details function changes to print json lines

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const port = 3000
 
 const yahooFinance = require('yahoo-finance2').default
 let result;
+
 async function details(req,res){
 let symbol = 'AAPL';
-let queryOptions = {modules: ['price', 'summaryDetail']};
 result = await yahooFinance.quoteSummary(symbol,{modules:["balanceSheetHistory"]});
-console.log(JSON.stringify(result))
+resultString = JSON.stringify(result)
+for(var i = 0; i < resultString.length; i++){
+  console.log(result[i]+ "\n");
+}
+
+
 
 }
 
@@ -22,7 +27,10 @@ app.get('/',(req, res) => {
     // middleware, because it is happening between request and response
   res.write('Hello World!')
  //res.send 
-   res.write(JSON.stringify(result))
+   
+   
+   
+
 
   
 })
